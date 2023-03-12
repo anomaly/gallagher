3c74e50b497101e4064a8edd3523ceac72ae5664
Dev Mukherjee <devraj@gmail.com>
2023-03-12 15:22:19 +1100

    refactor: root package simplified

--- gallagher/__init__.py
+++ gallagher/__init__.py
@@ -1,41 +1,9 @@
-"""" Gallagher Python bindings
+"""" Gallagher Python idiomatic client
 
-    After enabling the Gateway for your server and REST API user, you need 
-    only to replace the local server's URL with the cloud API Gateway one.
-    
-    Example Local request:
+ Copyight (c) 2023, Anomaly Software Pty Ltd
 
-            <client certificate thumbprint e89ef121958...>
-            GET https://servername.yourcompany.local:8904/api/cardholders
-            Authorization: GGL-API-KEY F6F0-C8F0-
-        
-    Equivalent Gateway request:
-
-          <client certificate thumbprint e89ef121958...>
-          GET https://commandcentre-api-au.security.gallagher.cloud/api/cardholders
-          Authorization: GGL-API-KEY F6F0-C8F0-60AF-
-
-    The API client takes of appending the headers onto the requests.
-
+ Distributed under the terms of the MIT License.
 """
-
-from typing import Optional
-
-from .const import URL
 
 __version__ = "0.1.0"
 
-# Follow the instructions in the Gallagher documentation
-# to obtain an API key
-api_key: str = None
-
-# By default the base API is set to the Australian Gateway
-# Override this with the US gateway or a local DNS/IP address
-api_base: str = URL.CLOUD_GATEWAY_AU
-
-# Default is set to the library, set this to your application
-client_id: str = "gallagher-py"
-
-# By default connections are sent straight to the server
-# should you wish to use a proxy, set this to the proxy URL
-proxy: Optional[str] = None

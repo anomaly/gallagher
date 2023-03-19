934cc331cab11cad7b1feefa9fda083cd4993d52
Dev Mukherjee <devraj@gmail.com>
2023-03-20 09:52:29 +1100

    fix: rules for min and max fields

---
+++ gallagher/schema/card_type.py
@@ -0,0 +1,24 @@
+"""
+
+
+"""
+
+from typing import Optional
+
+from .utils import AppBaseModel, IdentityMixin,\
+    HrefMixin
+
+class CardTypeDetail(
+    AppBaseModel,
+    IdentityMixin,
+    HrefMixin
+):
+    """ Card Types are cards mobile or physical that are supported at a site
+    """
+    name: str
+    minimum_number: Optional[str]
+    maximum_number: Optional[str]
+    initial_card_state: str
+    facility_code: str
+    credential_class: str
+    available_card_states: list[str]

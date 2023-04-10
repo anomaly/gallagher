ce1e506309856e46a467d1bc68f8dcec3638e9a8
Dev Mukherjee <devraj@gmail.com>
2023-04-10 14:14:17 +1000

    fix: checks for api key format

--- gallagher/cc/utils.py
+++ gallagher/cc/utils.py
@@ -1,15 +1,45 @@
 """
 
 """
+import httpx
 
-def check_api_key_format():
+from ..schema import Response
+
+def check_api_key_format(api_key):
     """
     
     """
-    return False
+    api_tokens = api_key.split('-')
+    return (api_tokens.count() == 8)
 
 def get_authorization_headers():
+    """
+    
+    """
     from . import api_key
     return {
-        "Authorization": f'GGL-API-KEY {api_key}'
+        'Authorization': f'GGL-API-KEY {api_key}'
     }
+
+class APIBase():
+    """
+    
+    """
+
+    ENDPOINT = None
+    RESPONSE_CLASS = None
+
+    TOP = 10
+    SORT = "id" # Can be set to id or -id
+    FIELDS = []
+
+    @classmethod
+    def list(cls, skip=0):
+        from . import api_base
+        response = httpx.get(
+            f'{api_base}{cls.ENDPOINT}',
+            headers=get_authorization_headers(),
+        )
+
+        card_holder = Response.parse_obj(response.json())
+        return card_holder

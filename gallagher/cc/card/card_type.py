38eafa13df83744857a7f8cf2d23dbe8ff8b0b28
Dev Mukherjee <devraj@gmail.com>
2023-04-10 14:13:35 +1000

    feat: adds `card_type` handler

--- gallagher/cc/card/card_type.py
+++ gallagher/cc/card/card_type.py
@@ -1,22 +1,9 @@
-import httpx
 
-from ...schema import CardTypeDetail, Response
-from ..utils import get_authorization_headers
+from ..utils import APIBase
 
-class CardType():
+class CardType(APIBase):
 
     ENDPOINT = "card_types"
-    
-    @classmethod
-    def list(cls, skip=0):
-        from .. import api_base
-        response = httpx.get(
-            f'{api_base}{cls.ENDPOINT}',
-            headers=get_authorization_headers(),
-        )
-
-        card_holder = Response.parse_obj(response.json())
-        return card_holder
 
     @classmethod
     def retrieve(cls, id):

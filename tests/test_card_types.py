2b532d7f75ca5ee7e174fa8bcc19f2fab57cceb0
Dev Mukherjee <devraj@gmail.com>
2023-11-12 17:09:50 +1100

    fix: import post package refactor

--- tests/test_card_types.py
+++ tests/test_card_types.py
@@ -4,7 +4,7 @@
 
 
 def test_get_card_types():
-    from gallagher.cc.card.card_type import CardType
+    from gallagher.cc.cardholders.card_type import CardType
     from gallagher.dto.card_type import CardTypeResponse
 
     response = CardType.list()

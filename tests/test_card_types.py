bcebcc528f04da35dd40fb4f54ca381df1711735
Dev Mukherjee <devraj@gmail.com>
2023-11-12 14:04:36 +1100

    fix: imports post package name change

--- tests/test_card_types.py
+++ tests/test_card_types.py
@@ -2,9 +2,10 @@
 
 """
 
+
 def test_get_card_types():
     from gallagher.cc.card.card_type import CardType
-    from gallagher.schema.card_type import CardTypeResponse
+    from gallagher.dto.card_type import CardTypeResponse
 
     response = CardType.list()
     assert type(response) is CardTypeResponse

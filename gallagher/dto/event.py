7a8db13bcc7a2094a713c21476cb0526b6378a6e
Dev Mukherjee <devraj@gmail.com>
2023-12-15 16:31:27 +1100

    feat: adds dto for event type/group feature

--- gallagher/dto/event.py
+++ gallagher/dto/event.py
@@ -10,10 +10,12 @@ from .utils import (
 )
 
 
-class EventType(
+class EventTypeSummary(
     AppBaseModel,
     IdentityMixin,
 ):
+    """ An event type has identifiers and names
+    """
     name: str
 
 
@@ -35,3 +37,28 @@ class EventSummary(
     next: HrefMixin
     previous: HrefMixin
     updates: HrefMixin
+
+
+class EventGroupSummary(
+    AppBaseModel,
+    IdentityMixin,
+):
+    """ Event Groups are a collection of event types
+
+    Each group has names and event types. This is usually used
+    in an Event Type Response.
+    """
+    name: str
+    event_types: list[EventTypeSummary]
+
+
+class EventTypeResponse(
+    AppBaseModel,
+):
+    """ Event Type Response
+
+    Event Type Responses return a set of eventGroups which in turn
+    has identifiers, names and event types.
+
+    """
+    event_groups: list[EventGroupSummary]

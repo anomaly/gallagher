fadb74e60d73efce3857bf8d8b0bda7725e79822
Dev Mukherjee <devraj@gmail.com>
2023-12-20 15:44:33 +1100

    refactor: wire up response classes at module level

--- gallagher/dto/response/__init__.py
+++ gallagher/dto/response/__init__.py
@@ -2,3 +2,41 @@
 
 
 """
+
+from .alarm import (
+    AlarmResponse,
+)
+
+from .card_type import (
+    CardTypeResponse,
+)
+
+from .cardholder import (
+    CardholderSummaryResponse,
+)
+
+from .day_category import (
+    DayCategoryResponse,
+)
+
+from .discover import (
+    DiscoveryResponse,
+)
+
+from .division import (
+    DivisionDetailResponse,
+)
+
+from .door import (
+    DoorSummaryResponse,
+)
+
+from .event import (
+    EventSummaryResponse,
+    EventTypeResponse,
+)
+
+from .items import (
+    ItemsSummaryResponse,
+    ItemTypesResponse,
+)

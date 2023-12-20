c952888f65f2f83d13a290533c82158265ccd9c5
Dev Mukherjee <devraj@gmail.com>
2023-12-20 15:44:24 +1100

    refactor: wire up ref classes at module level

--- gallagher/dto/ref/__init__.py
+++ gallagher/dto/ref/__init__.py
@@ -15,3 +15,69 @@ mostly because of the nature of the data that the command centre exposes.
 from .access_group import (
     AccessGroupRef
 )
+
+from .alarm import (
+    AlarmRef,
+    AlarmZoneRef,
+)
+
+from .cardholder import (
+    CardholderRef,
+)
+
+from .day_category import (
+    DayCategoryRef,
+)
+
+from .discover import (
+    FeatureAccessGroupsRef,
+    FeatureAccessZonesRef,
+    FeatureAlarmZonesRef,
+    FeatureAlarmsRef,
+    FeatureCardTypesRef,
+    FeatureCardholdersRef,
+    FeatureCompetenciesRef,
+    FeatureDayCategoriesRef,
+    FeatureDivisionsRef,
+    FeatureDoorsRef,
+    FeatureElevatorsRef,
+    FeatureEventsRef,
+    FeatureFenceZonesRef,
+    FeatureInputsRef,
+    FeatureInterlockGroupsRef,
+    FeatureItemsRef,
+    FeatureLockerBanksRef,
+    FeatureMacrosRef,
+    FeatureOperatorGroupsRef,
+    FeatureOutputsRef,
+    FeaturePersonalDataFieldsRef,
+    FeatureReceptionsRef,
+    FeatureRolesRef,
+    FeatureSchedulesRef,
+    FeatureVisitsRef,
+)
+
+from .door import (
+    DoorRef,
+)
+
+from .items import (
+    ItemRef,
+)
+
+from .pdf import (
+    PDFRef,
+)
+
+from .salto import (
+    SaltoItemTypeRef,
+    SaltoItemRef,
+)
+
+from .schedule import (
+    ScheduleRef,
+)
+
+from .zone import (
+    AccessZoneRef,
+)

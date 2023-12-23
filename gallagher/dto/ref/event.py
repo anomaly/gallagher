8d3e93c5b8e8f3a00b7311239e68ac4eba8ea49f
Dev Mukherjee <devraj@gmail.com>
2023-12-23 15:52:11 +1100

    refactor: removes unsolicitated code

--- gallagher/dto/ref/event.py
+++ gallagher/dto/ref/event.py
@@ -4,12 +4,3 @@ from ..utils import (
     AppBaseModel,
     IdentityMixin,
 )
-
-
-class EventTypeSummary(
-    AppBaseModel,
-    IdentityMixin,
-):
-    """ An event type has identifiers and names
-    """
-    name: str

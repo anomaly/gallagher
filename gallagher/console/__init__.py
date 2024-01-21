5a6ae43701a65066c52f4b0dc52c43752e7b7358
Dev Mukherjee <devraj@gmail.com>
2024-01-22 10:03:56 +1100

    feat: adds runner for textual console REFS #25

--- gallagher/console/__init__.py
+++ gallagher/console/__init__.py
@@ -1,28 +1,54 @@
 """ Console 
 """
 
-from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.app import (
+    App,
+    ComposeResult
+)
+
+from textual.containers import (
+    Container,
+    Horizontal,
+    Grid,
+)
+
+from textual.widgets import (
+    Header,
+    Footer,
+    Placeholder,
+)
 
 
 class GallagherConsole(App):
-    """A Textual app to manage stopwatches."""
+    """ A console interface for the Gallagher Command Centre.
+    """
+
+    CSS_PATH = "gallagher.tcss"
 
     BINDINGS = [
-        ("d", "toggle_dark", "Toggle dark mode")
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("ctrl+q", "quit", "Quit")
     ]
 
-    def on_mount(self) -> None:
-        self.title = "Gallagher"
-        self.sub_title = "Super charged textual console"
+    # Decorative constants
+    TITLE = "Gallagher"
+    SUB_TITLE = "Super charged textual console"
+
+    # def on_mount(self) -> None:
 
     def compose(self) -> ComposeResult:
-        """Create child widgets for the app."""
+        """ Create child widgets for the app.
+        """
         yield Header()
+        with Grid():
+            yield Placeholder(id="a")
+            yield Placeholder(id="b")
+            yield Placeholder(id="c")
         yield Footer()
 
     def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
+        """ An action to toggle dark mode.
+        """
         self.dark = not self.dark
 
 
---
+++ gallagher/console/overview.py
@@ -0,0 +1,11 @@
+"""
+
+
+"""
+from textual.containers import (
+    Container,
+)
+
+
+class Dashboard(Container):
+    pass

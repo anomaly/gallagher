a52d5f22e4688b990b1a37aaa5099b90d036e249
Dev Mukherjee <devraj@gmail.com>
2024-04-25 09:11:29 +1000

    chore: adds placeholder package for sql interfaces

---
+++ gallagher/ext/shillelagh/__init__.py
@@ -0,0 +1,8 @@
+""" Adapter and SQLAlchemy dialect for shillelagh
+
+The package provides extensions to provide a SQL interface for the
+Gallagher API.
+
+[shillelagh](https://github.com/betodealmeida/shillelagh)
+"""
+

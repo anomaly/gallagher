3335ae77e8cb45a7446b4e2b22afaeddfcce3027
Dev Mukherjee <devraj@gmail.com>
2024-04-25 09:12:32 +1000

    chore: adds init for ext package

---
+++ gallagher/ext/__init__.py
@@ -0,0 +1,14 @@
+""" Extensions package for building upon other frameworks
+
+This package provides extensions around framewokrs like shillelagh
+that allow us to extend functionality like a SQL interface to
+the package.
+
+
+It's recommdend that we list the package that ext provides for 
+developers to understand what is possible and in what context.
+
+- [shillelagh](https://youtu.be/85ztwhfyQ-k?feature=shared&t=14) 
+  how to pronounce
+
+"""

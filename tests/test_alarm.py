5a04ae1141ed55c31c6d5dbb962f6888d4cb17bd
Dev Mukherjee <devraj@gmail.com>
2024-04-30 14:44:16 +1000

    test: adds test for alarm detail
    changes various class names in accordance with the previous commit and
    adds detail check for alarms

--- tests/test_alarm.py
+++ tests/test_alarm.py
@@ -10,33 +10,44 @@ async def test_alarms_list():
 
     """
     from gallagher.cc.alarms import (
-        Alarms
+        Alarms,
     )
     from gallagher.dto.response import (
-        AlarmResponse
+        AlarmSummaryResponse,
     )
 
     response = await Alarms.list()
-    assert type(response) is AlarmResponse
+    assert type(response) is AlarmSummaryResponse
     assert type(response.alarms) is list
     assert len(response.alarms) > 0
 
 
-# async def test_alarms_detail():
-#     """ Get a list of alarms and then try and get it's detail
-#     """
-#     from gallagher.cc.alarms import (
-#         Alarms
-#     )
-#     from gallagher.dto.response import (
-#         AlarmResponse,
-#     )
+async def test_alarms_detail():
+    """ Get a list of alarms and then try and get it's detail
+    """
+    from gallagher.cc.alarms import (
+        Alarms
+    )
 
-#     response = await Alarms.list()
-#     assert type(response) is AlarmResponse
-#     assert type(response.alarms) is list
-#     assert len(response.alarms) > 0
+    from gallagher.dto.response import (
+        AlarmSummaryResponse,
+    )
 
-#     for alarm in response.results:
-#         pass
+    from gallagher.dto.detail import (
+        AlarmDetail,
+    )
+
+    response = await Alarms.list()
+    assert type(response) is AlarmSummaryResponse
+    assert type(response.alarms) is list
+    assert len(response.alarms) > 0
+
+    for alarm_summary in response.alarms:
+        # Get the detail of the alarm for comparison
+        alarm_detail_response = await Alarms.retrieve(
+            alarm_summary.id
+        )
+        assert type(alarm_detail_response) is AlarmDetail
+        # assert (alarm_detail_response.id == alarm_summary.id)
+
 

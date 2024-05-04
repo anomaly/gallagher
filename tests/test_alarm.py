695e3fdc1d43836221dad83a1998c364f6cd4a41
Dev Mukherjee <devraj@gmail.com>
2024-05-04 17:19:09 +1000

    refactor: splits out fetch and compare

--- tests/test_alarm.py
+++ tests/test_alarm.py
@@ -6,55 +6,18 @@ from datetime import datetime
 
 import pytest
 
-async def test_alarms_list():
-    """ Get a list of item types and iterates through it
-    these are a summary response
 
-    """
-    from gallagher.cc.alarms import (
-        Alarms,
-    )
-    from gallagher.dto.response import (
-        AlarmSummaryResponse,
-    )
+from gallagher.dto.detail import (
+    AlarmDetail,
+)
 
-    response = await Alarms.list()
-    assert type(response) is AlarmSummaryResponse
-    assert type(response.alarms) is list
-    assert len(response.alarms) > 0
+from gallagher.dto.response import (
+    AlarmSummaryResponse,
+)
 
-
-async def test_alarms_detail():
-    """ Get details of all the alarms 
-
-    This will get the details of all the alarms and then
-    compare the summary with the detail to make sure that
-    the data is consistent.
-    """
-    from gallagher.cc.alarms import (
-        Alarms
-    )
-
-    from gallagher.dto.response import (
-        AlarmSummaryResponse,
-    )
-
-    from gallagher.dto.detail import (
-        AlarmDetail,
-    )
-
-    response = await Alarms.list()
-    assert type(response) is AlarmSummaryResponse
-    assert type(response.alarms) is list
-    assert len(response.alarms) > 0
-
-    for alarm_summary in response.alarms:
-        # Get the detail of the alarm for comparison
-        alarm_detail_response = await Alarms.retrieve(
-            alarm_summary.id
-        )
-        assert type(alarm_detail_response) is AlarmDetail
-        assert (alarm_detail_response.id == alarm_summary.id)
+from gallagher.cc.alarms import (
+    Alarms,
+)
 
 # Fixture for the comment, this will create a timestamp
 # based string so we can compare this is a comment that
@@ -64,30 +27,56 @@ async def test_alarms_detail():
 async def alarm_comment():
     return f"Test suite alarm comment {datetime.now()}"
 
-async def test_alarms_post_comment(alarm_comment: str):
+
+@pytest.fixture
+async def alarm_summary() -> AlarmSummaryResponse:
+    """ Get a list of item types and iterates through it
+    these are a summary response
+
+    """
+    response = await Alarms.list()
+    return response
+
+async def test_alarms_list(
+    alarm_summary: AlarmSummaryResponse
+):
+    """ Get a list of item types and iterates through it
+    these are a summary response
+
+    """
+    assert type(alarm_summary) is AlarmSummaryResponse
+    assert type(alarm_summary.alarms) is list
+    assert len(alarm_summary.alarms) > 0
+
+
+async def test_alarms_detail(
+    alarm_summary: AlarmSummaryResponse
+):
+    """ Get details of all the alarms 
+
+    This will get the details of all the alarms and then
+    compare the summary with the detail to make sure that
+    the data is consistent.
+    """
+
+    for alarm_summary in alarm_summary.alarms:
+        # Get the detail of the alarm for comparison
+        alarm_detail_response = await Alarms.retrieve(
+            alarm_summary.id
+        )
+        assert type(alarm_detail_response) is AlarmDetail
+        assert (alarm_detail_response.id == alarm_summary.id)
+
+async def test_alarms_post_comment(
+    alarm_summary: AlarmSummaryResponse,
+    alarm_comment: str
+):
     """ Posts a commend to an alarm 
 
     This will make a new comment and then expect the next test
     to figure out that we have a new comment on the first
     """
-    from gallagher.cc.alarms import (
-        Alarms
-    )
-
-    from gallagher.dto.response import (
-        AlarmSummaryResponse,
-    )
-
-    from gallagher.dto.detail import (
-        AlarmDetail,
-    )
-
-    response = await Alarms.list()
-    assert type(response) is AlarmSummaryResponse
-    assert type(response.alarms) is list
-    assert len(response.alarms) > 0
-
-    for alarm_summary in response.alarms:
+    for alarm_summary in alarm_summary.alarms:
         # Get the detail of the alarm for comparison
         await Alarms.comment(
             alarm_summary,

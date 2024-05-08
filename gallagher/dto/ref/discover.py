df100d3ede8b9120327097967a5b82a97b73675e
Dev Mukherjee <devraj@gmail.com>
2024-05-08 13:06:24 +1000

    refactor: assigns none rather than a model instance

--- gallagher/dto/ref/discover.py
+++ gallagher/dto/ref/discover.py
@@ -17,158 +17,158 @@ from ..utils import (
 class FeatureAccessGroupsRef(
     AppBaseModel,
 ):
-    access_groups: OptionalHrefMixin = OptionalHrefMixin()
+    access_groups: OptionalHrefMixin = None
 
 
 class FeatureAccessZonesRef(
     AppBaseModel,
 ):
-    access_zones: OptionalHrefMixin = OptionalHrefMixin()
+    access_zones: OptionalHrefMixin = None
 
 
 class FeatureAlarmZonesRef(
     AppBaseModel,
 ):
-    alarm_zones: OptionalHrefMixin = OptionalHrefMixin()
+    alarm_zones: OptionalHrefMixin = None
 
 
 class FeatureAlarmsRef(
     AppBaseModel,
 ):
-    alarms: OptionalHrefMixin = OptionalHrefMixin()
-    divisions: OptionalHrefMixin = OptionalHrefMixin()
-    updates: OptionalHrefMixin = OptionalHrefMixin()
+    alarms: OptionalHrefMixin = None
+    divisions: OptionalHrefMixin = None
+    updates: OptionalHrefMixin = None
 
 
 class FeatureCardTypesRef(
     AppBaseModel,
 ):
-    assign: OptionalHrefMixin = OptionalHrefMixin()
-    card_types: OptionalHrefMixin = OptionalHrefMixin()
+    assign: OptionalHrefMixin = None
+    card_types: OptionalHrefMixin = None
 
 
 class FeatureCardholdersRef(
     AppBaseModel,
 ):
-    cardholders: OptionalHrefMixin = OptionalHrefMixin()
-    changes: OptionalHrefMixin = OptionalHrefMixin()
-    update_location_access_zones: OptionalHrefMixin = OptionalHrefMixin()
+    cardholders: OptionalHrefMixin = None
+    changes: OptionalHrefMixin = None
+    update_location_access_zones: OptionalHrefMixin = None
 
 
 class FeatureCompetenciesRef(
     AppBaseModel,
 ):
-    competencies: OptionalHrefMixin = OptionalHrefMixin()
+    competencies: OptionalHrefMixin = None
 
 
 class FeatureDayCategoriesRef(
     AppBaseModel,
 ):
-    day_categories: OptionalHrefMixin = OptionalHrefMixin()
+    day_categories: OptionalHrefMixin = None
 
 
 class FeatureDivisionsRef(
     AppBaseModel,
 ):
-    divisions: OptionalHrefMixin = OptionalHrefMixin()
+    divisions: OptionalHrefMixin = None
 
 
 class FeatureDoorsRef(
     AppBaseModel,
 ):
-    doors: OptionalHrefMixin = OptionalHrefMixin()
+    doors: OptionalHrefMixin = None
 
 
 class FeatureElevatorsRef(
     AppBaseModel,
 ):
-    elevator_groups: OptionalHrefMixin = OptionalHrefMixin()
+    elevator_groups: OptionalHrefMixin = None
 
 
 class FeatureEventsRef(
     AppBaseModel,
 ):
-    divisions: OptionalHrefMixin = OptionalHrefMixin()
-    event_groups: OptionalHrefMixin = OptionalHrefMixin()
-    events: OptionalHrefMixin = OptionalHrefMixin()
-    updates: OptionalHrefMixin = OptionalHrefMixin()
+    divisions: OptionalHrefMixin = None
+    event_groups: OptionalHrefMixin = None
+    events: OptionalHrefMixin = None
+    updates: OptionalHrefMixin = None
 
 
 class FeatureFenceZonesRef(
     AppBaseModel,
 ):
-    fence_zones: OptionalHrefMixin = OptionalHrefMixin()
+    fence_zones: OptionalHrefMixin = None
 
 
 class FeatureInputsRef(
     AppBaseModel,
 ):
-    inputs: OptionalHrefMixin = OptionalHrefMixin()
+    inputs: OptionalHrefMixin = None
 
 
 class FeatureInterlockGroupsRef(
     AppBaseModel,
 ):
-    interlock_groups: OptionalHrefMixin = OptionalHrefMixin()
+    interlock_groups: OptionalHrefMixin = None
 
 
 class FeatureItemsRef(
     AppBaseModel,
 ):
-    item_types: OptionalHrefMixin = OptionalHrefMixin()
-    items: OptionalHrefMixin = OptionalHrefMixin()
-    updates: OptionalHrefMixin = OptionalHrefMixin()
+    item_types: OptionalHrefMixin = None
+    items: OptionalHrefMixin = None
+    updates: OptionalHrefMixin = None
 
 
 class FeatureLockerBanksRef(
     AppBaseModel,
 ):
-    locker_banks: OptionalHrefMixin = OptionalHrefMixin()
+    locker_banks: OptionalHrefMixin = None
 
 
 class FeatureMacrosRef(
     AppBaseModel,
 ):
-    macros: OptionalHrefMixin = OptionalHrefMixin()
+    macros: OptionalHrefMixin = None
 
 
 class FeatureOperatorGroupsRef(
     AppBaseModel,
 ):
-    operator_groups: OptionalHrefMixin = OptionalHrefMixin()
+    operator_groups: OptionalHrefMixin = None
 
 
 class FeatureOutputsRef(
     AppBaseModel,
 ):
-    outputs: OptionalHrefMixin = OptionalHrefMixin()
+    outputs: OptionalHrefMixin = None
 
 
 class FeaturePersonalDataFieldsRef(
     AppBaseModel,
 ):
-    personal_data_fields: OptionalHrefMixin = OptionalHrefMixin()
+    personal_data_fields: OptionalHrefMixin = None
 
 
 class FeatureReceptionsRef(
     AppBaseModel,
 ):
-    receptions: OptionalHrefMixin = OptionalHrefMixin()
+    receptions: OptionalHrefMixin = None
 
 
 class FeatureRolesRef(
     AppBaseModel,
 ):
-    roles: OptionalHrefMixin = OptionalHrefMixin()
+    roles: OptionalHrefMixin = None
 
 
 class FeatureSchedulesRef(
     AppBaseModel,
 ):
-    schedules: OptionalHrefMixin = OptionalHrefMixin()
+    schedules: OptionalHrefMixin = None
 
 
 class FeatureVisitsRef(
     AppBaseModel,
 ):
-    visits: OptionalHrefMixin = OptionalHrefMixin()
+    visits: OptionalHrefMixin = None

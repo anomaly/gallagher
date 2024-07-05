9ef7376af595d23ade1f5ca99e54e8a919d6ce9f
Dev Mukherjee <devraj@gmail.com>
2024-07-05 16:45:11 +1000

    feat(cli): wire up the cardholder search
    wires up the cardholder search to the cli, demonstrating use of the enums
    proper annotations for typer to spell out the options on the interface
    this should serve as the template to wire up almost all searches
    
    refs #13

--- gallagher/cli/cardholders.py
+++ gallagher/cli/cardholders.py
@@ -2,6 +2,9 @@
 
 """
 
+from typing import Optional
+from typing_extensions import Annotated
+
 import typer
 
 from rich.console import Console
@@ -9,6 +12,7 @@ from rich.table import Table
 
 from .utils import AsyncTyper
 
+from gallagher.enum import SearchSortOrder
 from gallagher.cc.cardholders import Cardholder
 
 from gallagher.exception import (
@@ -39,7 +43,9 @@ async def list():
 
 
 @app.command("get")
-async def get(id: int):
+async def get(
+    id: Annotated[int, typer.Argument(help='cardholder id')],
+):
     """get a cardholder by id"""
     console = Console()
     with console.status("[bold]Finding cardholder...", spinner="dots"):
@@ -50,3 +56,58 @@ async def get(id: int):
         except NotFoundException as e:
             console.print(f"[bold]No cardholder with id={id} found[/bold]")
             raise typer.Exit(code=1)
+
+@app.command("find")
+async def find(
+    name: Annotated[
+        str,
+        typer.Argument(help="First or last name to search for")
+    ],
+    sort: Annotated[
+        SearchSortOrder,
+        typer.Option(help='Sort order')
+    ] = SearchSortOrder.NAME,
+    description: Annotated[
+        Optional[str],
+        typer.Option(
+            '--desc',
+            help='Keywords to search in description'
+        )
+    ] = None,
+    top: Annotated[
+        int,
+        typer.Option(help='Results to fetch from top')
+    ] = 100,
+    div: Annotated[
+        Optional[int],
+        typer.Option(help='Division id for hierarchical search')
+    ] = None,
+    dirdiv: Annotated[
+        Optional[int],
+        typer.Option(
+            '--ddiv',
+            help='Division id for direct search'
+        )
+    ] = None,
+):
+    """find cardholders by name"""
+    console = Console()
+    with console.status("[bold]Searching cardholders...", spinner="dots"):
+
+        cardholders = await Cardholder.search(
+            name=name,
+            sort=sort,
+            top=top,
+            division=div,
+            division_dir=dirdiv,
+            description=description,
+        )
+
+        table = Table(title="Cardholders")
+        for header in cardholders.cli_header:
+            table.add_column(header)
+
+        for row in cardholders.__rich_repr__():
+            table.add_row(*row)
+
+        console.print(table)
